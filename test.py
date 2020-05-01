f6afefdac031206b1fbed19e81ed6739a90b8b9d
jordan5560 <jordanlhchow@gmail.com>
2020-04-30 20:46:16 -0700

    uploaded scrapper and run code

---
+++ test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 27 19:43:23 2020
+
+@author: jordan5560
+"""
+
+import glassdoor_scraper as gs
+import pandas as pd
+path = "/Users/jordan5560/Desktop/Projects/ds_salary_proj/chromedriver"
+
+df = gs.get_jobs('data scientist', 20, False, path, 20)
